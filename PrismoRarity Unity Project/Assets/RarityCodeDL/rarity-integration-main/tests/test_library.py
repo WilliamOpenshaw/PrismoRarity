23ef0dc7845fb24f27dd95731191bb9d7c38386b
William Openshaw <ophashias@gmail.com>
2021-10-11 22:39:25 +0800

    Nethereum Testing
    Added Nethereum
    Trying to read smart contracts with c#
    Will test until Rarity summoners stats can be called from Rarity smart contract.

---
+++ PrismoRarity Unity Project/Assets/RarityCodeDL/rarity-integration-main/tests/test_library.py
@@ -0,0 +1,75 @@
+s1 = 466576
+s2 = 466591
+s3 = 555555
+
+
+def test_names(lib):
+    assert lib.name(s1) == "Conan"
+    assert lib.description(s1) == "Conan, Level 2 Barbarian"
+
+
+def test_scores(lib):
+    assert lib.ability_scores(s1) == (18, 14, 14, 10, 10, 8)
+    assert lib.ability_scores_full(s1) == ((18, 14, 14, 10, 10, 8), (4, 2, 2, 0, 0, -1), 32, 32, True)
+    assert lib.ability_modifiers(s1) == (4, 2, 2, 0, 0, -1)
+
+
+def test_core(lib):
+    core = lib.base(s1)
+    assert core[0] == "Conan"
+    assert core[3] == 1
+    assert core[4] == 2
+
+
+def test_gold(lib):
+    assert lib.gold(s1) == (0, 1000e18, 0)
+
+
+def test_materials(lib):
+    mats = lib.materials(s1)
+    assert len(mats) == 1
+    assert mats[0][1] == 8
+
+
+def test_skills(lib):
+    skills = lib.skills(1947680)
+    assert sum(skills[0]) == 4
+    assert sum(skills[1]) == 6
+    assert skills[2:] == (32, 4)
+
+
+def test_full(lib):
+    lib.summoner_full(s1)
+    lib.summoners_full([s1, s2])
+
+
+def test_items(lib, owner):
+    items = lib.items1(owner)
+    assert items[0] == (36, 2, 12, 1631467054, 466789)
+    print(len(items))
+
+
+# def test_daycare(lib):
+#     misc = lib.misc(2454502)
+#     assert misc[0] == 2
+
+
+def test_print_full(lib):
+    print(lib.summoner_full(2454502))
+
+
+def test_not_owned_and_created(lib):
+    lib.base(s3)
+    lib.name(s3)
+    lib.description(s3)
+    lib.ability_scores(s3)
+    lib.ability_modifiers(s3)
+    lib.ability_scores_full(s3)
+    lib.skills(s3)
+    lib.gold(s3)
+    lib.materials(s3)
+    lib.summoner_full(s3)
+    lib.summoners_full([s3, 555556]*5)
+
+
+
---
+++ PrismoRarity Unity Project/Assets/Scripts/ExampleTestReadPythOrSol.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleTestReadPythOrSol : MonoBehaviour
+{
+    public int summonerIDNumber;
+    public int currentSummonerLevel;
+    public int pyTestSummonerIDNumber;
+    public int pyTestCurrentSummonerLevel;
+
+    //public conftest conftestPythScript;
+    //public attributes attributesSolScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+        summonerIDNumber = attributesSolScript.uint;
+        currentSummonerLevel = attributesSolScript.level;
+        pyTestSummonerIDNumber = conftestPythScript.account;
+        pyTestCurrentSummonerLevel = conftestPythScript.rm;
+        */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
